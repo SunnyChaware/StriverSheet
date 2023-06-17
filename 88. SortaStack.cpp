3b1bf0ccdf84c70377b4e636b6a9451c20ba832c
Cypher <47352505+SunnyChaware@users.noreply.github.com>
2023-06-17 22:17:33 +0530

    Create 88. SortaStack.cpp

---
+++ 88. SortaStack.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+void sortedInsert(stack<int> &stack, int current)
+{
+	if (stack.empty() || current > stack.top())
+	{
+		stack.push(current);
+		return;
+	}
+
+	int top = stack.top();
+	stack.pop();
+	sortedInsert(stack, current);
+	stack.push(top);
+}
+void sortStack(stack<int> &stack)
+{
+	if (stack.empty()){
+		return;
+	}
+
+	int top = stack.top();
+	stack.pop();
+
+	sortStack(stack);
+	sortedInsert(stack, top);
+
+}
