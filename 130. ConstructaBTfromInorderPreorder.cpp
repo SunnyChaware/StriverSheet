6dc52feec22961fce818e893ae6b25a0bebc0526
Cypher <47352505+SunnyChaware@users.noreply.github.com>
2023-07-03 21:00:14 +0530

    Create 130. ConstructaBTfromInorderPreorder.cpp

---
+++ 130. ConstructaBTfromInorderPreorder.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+TreeNode<int> * buildTree(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder,
+int inStart, int inEnd, map<int,int> &mp){
+    if(preStart > preEnd || inStart > inEnd)return NULL;
+
+    TreeNode<int> * root = new TreeNode<int> (preorder[preStart]);
+
+    int inRoot = mp[root->data];
+    int numsLeft = inRoot - inStart;
+
+    root->left = buildTree(preorder, preStart+1, preStart+numsLeft, inorder, inStart, inRoot-1, mp);
+    root->right = buildTree(preorder, preStart+numsLeft+1, preEnd, inorder, inRoot+1, inEnd, mp);
+
+    return root;
+}
+
+TreeNode<int> *buildBinaryTree(vector<int> &inorder, vector<int> &preorder)
+{
+	map<int, int> mp;
+    for(int i=0;i<inorder.size();i++){
+        mp[inorder[i]] = i;
+    }
+
+    TreeNode<int> * root = buildTree(preorder, 0, preorder.size()-1, 
+                                    inorder, 0, inorder.size()-1, mp);
+
+    return root;
+}
+
